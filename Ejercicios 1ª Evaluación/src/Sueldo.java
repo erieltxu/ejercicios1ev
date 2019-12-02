86a8806f45873ce9c739d7a5fa1242619ff3d2d4
Leire Del Hoyo <erieltxu88@gmail.com>
2019-12-02 20:11:08 +0100

    Ejercicio sueldo

--- Ejercicios 1ª Evaluación/src/Sueldo.java
+++ Ejercicios 1ª Evaluación/src/Sueldo.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Sueldo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		System.out.print("Sueldo base: ");
+		float sueldo = in.nextFloat();
+		System.out.print("Importe de la primera venta: ");
+		float venta1 = in.nextFloat();
+		System.out.print("Importe de la segunda venta: ");
+		float venta2 = in.nextFloat();
+		System.out.print("Importe de la tercera venta: ");
+		float venta3 = in.nextFloat();
+		float comision = (venta1 + venta2 + venta3) * 0.1f;
+		System.out.printf("Comisión por ventas: %.2f\nSueldo total: %.2f\n", comision, sueldo + comision);
+		in.close();
 	}
 
 }
