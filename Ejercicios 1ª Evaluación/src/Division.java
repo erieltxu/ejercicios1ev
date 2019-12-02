b9c7407fb95738be060bb4733791a8ae025951eb
Leire Del Hoyo <erieltxu88@gmail.com>
2019-12-02 19:30:55 +0100

    Ejercicio division

---
+++ Ejercicios 1ª Evaluación/src/Division.java
@@ -0,0 +1,19 @@
+
+public class Division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		double num1=1234;
+		double num2=532;
+		double resultado= num1/num2;
+		
+	
+		
+		System.out.printf("El resultado de la operación es: %15.2f" , resultado);
+		
+
+	}
+
+}
